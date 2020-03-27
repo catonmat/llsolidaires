3373d3bda486e166ddd2f75d2a75087d5ac48968
Paul Portier <paulportier@gmail.com>
2020-03-27 14:02:35 +0100

    bouton sos

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,19 @@
 class DashboardsController < ApplicationController
+
   def index
     @flats = Flat.all
   end
+
   def owner
     @flats = Flat.all
   end
+
   def medical
     @flats = Flat.all
+  end
+
+  def send_sos_mail
+    # Implémenter la méthode d'envoi de mail SOS
+    redirect_to root_path
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,8 @@ class PagesController < ApplicationController
   def home
    if user_signed_in?
       if current_user.role == "owner"
-        # ICI REMPLACER "redirect_to root_path" PAR LA ROUTE VERS DASHBOARD PROPRIETAIRES
         redirect_to dashboards_owner_path
       elsif current_user.role == "medical"
-        # ICI REMPLACER "redirect_to root_path" PAR LA ROUTE VERS DASHBOARD PERSONNEL SOIGNANT
         redirect_to dashboards_medical_path
       end
     end
